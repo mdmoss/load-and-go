94c656a9a4f62fda00fb4936efb49cd09e03ef23
Matthew Moss <mdm@cse.unsw.edu.au>
2014-03-20 16:40:51 +1100

    Refactor a little in the clear light of day

--- htmltocsv.py
+++ htmltocsv.py
@@ -6,27 +6,26 @@ import csv
 from bs4 import BeautifulSoup
 
 def row_to_dict(row):
-    date_str = row.find(id = re.compile(r'transactionView\.output\.transactionDate\d+')).get_text()
-    date = " ".join(date_str.split()[0:3])
+    date_tag = row.find(id = re.compile(r'transactionView\.output\.transactionDate\d+'))
+    date = " ".join(date_tag.get_text().split()[0:3])
 
-    ref_str = row.find(id = re.compile(r'transactionView\.output\.reference\d+')).get_text()
-    ref = " ".join(ref_str.split())
+    ref_tag = row.find(id = re.compile(r'transactionView\.output\.reference\d+')).get_text()
+    ref = " ".join(ref_tag.get_text().split())
 
-    desc_str = row.find(id = re.compile(r'transactionView\.output\.transactionDescription\d+')).get_text()
-    desc = " ".join(desc_str.split())
+    desc_tag = row.find(id = re.compile(r'transactionView\.output\.transactionDescription\d+'))
+    desc = " ".join(desc_tag.get_text().split())
 
     maybe_money_in = row.find(id = re.compile(r'transactionView\.output\.moneyIn\d+'))
     if (maybe_money_in and re.search("\d+\.\d\d", maybe_money_in.get_text())):
-        delta = '+' + maybe_money_in.get_text().strip()
+        amount = '+' + maybe_money_in.get_text().strip()
     else:
         money_out = row.find(id = re.compile(r'transactionView\.output\.moneyOut\d+'))
-        delta = '-' + money_out.get_text().strip()
+        amount = '-' + money_out.get_text().strip()
     
-    balance_str = row.find(id = re.compile(r'transactionView\.output\.total\d+')).get_text()
-    balance = " ".join(balance_str.split())
-     
+    balance_tag = row.find(id = re.compile(r'transactionView\.output\.total\d+'))
+    balance = " ".join(balance_str.get_text().split())
 
-    return {'date':date, 'ref':ref, 'desc':desc, 'delta':delta, 'balance':balance}
+    return {'date':date, 'ref':ref, 'desc':desc, 'amount':amount, 'balance':balance}
 
 html = sys.stdin.read()
 soup = BeautifulSoup(html)
@@ -38,5 +37,5 @@ with open('out.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['date', 'reference', 'description', 'amount', 'balance'])
     for r in output:
-        writer.writerow([r['date'], r['ref'], r['desc'], r['delta'], r['balance']])
+        writer.writerow([r['date'], r['ref'], r['desc'], r['amount'], r['balance']])
 
